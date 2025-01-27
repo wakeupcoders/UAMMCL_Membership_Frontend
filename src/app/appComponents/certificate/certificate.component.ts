63b5f3d15dbc48380dc0eb52ac116dac7a07ca65
Narayan Jha <wakeupcoders@gmail.com>
2025-01-27 18:20:19 +0530

    certificate and other improvements.

--- src/app/appComponents/certificate/certificate.component.ts
+++ src/app/appComponents/certificate/certificate.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GenericService } from 'src/app/appServices/generic.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-certificate',
@@ -30,5 +32,35 @@ export class CertificateComponent implements OnInit {
   }
 
 
+  downloadPDF() {
+    const content = document.getElementById('content') as HTMLElement;
+
+    html2canvas(content, {
+      scale: 2, // High-resolution scaling
+      useCORS: true, // Enable cross-origin image loading
+    }).then((canvas) => {
+      const imgWidth = 210; // A4 width in mm
+      const pageHeight = 297; // A4 height in mm
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const imgData = canvas.toDataURL('image/png');
+    
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+      pdf.save('Certificate.pdf');
+    });
+    
+
+    // html2canvas(content, { scale: 2 }).then((canvas) => {
+    
+    //   const imgWidth = 210; // A4 width in mm
+    //   const pageHeight = 297; // A4 height in mm
+    //   const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    //   const imgData = canvas.toDataURL('image/png');
+
+    //   const pdf = new jsPDF('p', 'mm', 'a4');
+    //   pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+    //   pdf.save('Certificate.pdf');
+    // });
+  }
 
 }
